3767bf832dd00c05de297947b8af230ba0fb28ff
Dee Vazquez <dee.quez11@gmail.com>
2022-06-13 16:14:35 -0700

    Fix scss import paths to cpsitter

--- scss/binding_test.go
+++ scss/binding_test.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"testing"
 
-	sitter "github.com/smacker/go-tree-sitter"
-	"github.com/smacker/go-tree-sitter/scss"
+	sitter "github.com/codepen/go-tree-sitter"
+	"github.com/codepen/go-tree-sitter/scss"
 	"github.com/stretchr/testify/assert"
 )
 
